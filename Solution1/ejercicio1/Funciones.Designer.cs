9dda1c6885362d408767f218459853fc1051cfa6
Kristian-Blanco <kristianblanco94@gmail.com>
2021-09-11 08:18:18 -0600

    Procedimientos y funciones

---
+++ Solution1/ejercicio1/Funciones.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace ejercicio1
+{
+    partial class Funciones
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_numero1 = new System.Windows.Forms.TextBox();
+            this.txt_numero2 = new System.Windows.Forms.TextBox();
+            this.btn_suma = new System.Windows.Forms.Button();
+            this.btn_multiplicar = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_dividir = new System.Windows.Forms.Button();
+            this.btn_resta = new System.Windows.Forms.Button();
+            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_numero2);
+            this.groupBox1.Controls.Add(this.txt_numero1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(26, 55);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(314, 139);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Valores";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_resta);
+            this.groupBox2.Controls.Add(this.btn_dividir);
+            this.groupBox2.Controls.Add(this.btn_limpiar);
+            this.groupBox2.Controls.Add(this.btn_multiplicar);
+            this.groupBox2.Controls.Add(this.btn_suma);
+            this.groupBox2.Location = new System.Drawing.Point(385, 55);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(336, 139);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Operaciones";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lbl_resultado);
+            this.groupBox3.Location = new System.Drawing.Point(26, 220);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(456, 100);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Resultados";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero 2";
+            // 
+            // txt_numero1
+            // 
+            this.txt_numero1.Location = new System.Drawing.Point(105, 22);
+            this.txt_numero1.Name = "txt_numero1";
+            this.txt_numero1.Size = new System.Drawing.Size(128, 20);
+            this.txt_numero1.TabIndex = 2;
+            // 
+            // txt_numero2
+            // 
+            this.txt_numero2.Location = new System.Drawing.Point(105, 69);
+            this.txt_numero2.Name = "txt_numero2";
+            this.txt_numero2.Size = new System.Drawing.Size(128, 20);
+            this.txt_numero2.TabIndex = 3;
+            // 
+            // btn_suma
+            // 
+            this.btn_suma.Location = new System.Drawing.Point(35, 30);
+            this.btn_suma.Name = "btn_suma";
+            this.btn_suma.Size = new System.Drawing.Size(75, 23);
+            this.btn_suma.TabIndex = 0;
+            this.btn_suma.Text = "Suma";
+            this.btn_suma.UseVisualStyleBackColor = true;
+            this.btn_suma.Click += new System.EventHandler(this.btn_suma_Click);
+            // 
+            // btn_multiplicar
+            // 
+            this.btn_multiplicar.Location = new System.Drawing.Point(35, 67);
+            this.btn_multiplicar.Name = "btn_multiplicar";
+            this.btn_multiplicar.Size = new System.Drawing.Size(75, 23);
+            this.btn_multiplicar.TabIndex = 1;
+            this.btn_multiplicar.Text = "Multiplicar";
+            this.btn_multiplicar.UseVisualStyleBackColor = true;
+            this.btn_multiplicar.Click += new System.EventHandler(this.btn_multiplicar_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Location = new System.Drawing.Point(106, 110);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(75, 23);
+            this.btn_limpiar.TabIndex = 2;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_dividir
+            // 
+            this.btn_dividir.Location = new System.Drawing.Point(173, 64);
+            this.btn_dividir.Name = "btn_dividir";
+            this.btn_dividir.Size = new System.Drawing.Size(75, 23);
+            this.btn_dividir.TabIndex = 3;
+            this.btn_dividir.Text = "Dividir";
+            this.btn_dividir.UseVisualStyleBackColor = true;
+            this.btn_dividir.Click += new System.EventHandler(this.btn_dividir_Click);
+            // 
+            // btn_resta
+            // 
+            this.btn_resta.Location = new System.Drawing.Point(173, 30);
+            this.btn_resta.Name = "btn_resta";
+            this.btn_resta.Size = new System.Drawing.Size(75, 23);
+            this.btn_resta.TabIndex = 4;
+            this.btn_resta.Text = "Resta";
+            this.btn_resta.UseVisualStyleBackColor = true;
+            this.btn_resta.Click += new System.EventHandler(this.btn_resta_Click);
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.AutoSize = true;
+            this.lbl_resultado.Location = new System.Drawing.Point(202, 44);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(55, 13);
+            this.lbl_resultado.TabIndex = 2;
+            this.lbl_resultado.Text = "Resultado";
+            // 
+            // Funciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Funciones";
+            this.Text = "Funciones y procedimientos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_numero2;
+        private System.Windows.Forms.TextBox txt_numero1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_resta;
+        private System.Windows.Forms.Button btn_dividir;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_multiplicar;
+        private System.Windows.Forms.Button btn_suma;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lbl_resultado;
+    }
+}
+
---
+++ Solution1/ejercicio1/Funciones.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio1
+{
+    public partial class Funciones : Form
+    {
+        public Funciones()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarControles();
+        }
+
+        //procedimiento no devuelven valores (void no devuelve nada)
+        private void LimpiarControles()
+        {
+        //->Limpiar texbox
+            txt_numero1.Clear();
+            txt_numero2.Text=""; 
+            //txt_numero1.text=string.empty:
+        }
+         
+
+        //Tipo de dato que va a devolver
+        private int Sumar(int valor1, int valor2) //se puede colocar parametros em los parentesis
+        {
+           return valor1 + valor2;
+        }
+
+        private void btn_suma_Click(object sender, EventArgs e)
+        {
+            lbl_resultado.Text = Sumar(Convert.ToInt32(txt_numero1.Text), Convert.ToInt32(txt_numero2.Text)).ToString();
+        }
+
+
+
+
+
+        private int Resta(int valor1, int valor2) //se puede colocar parametros em los parentesis
+        {
+            int resta = valor1 - valor2;
+            return resta;
+        }
+
+        private void btn_resta_Click(object sender, EventArgs e)
+        {
+            lbl_resultado.Text = Resta(Convert.ToInt32(txt_numero1.Text), Convert.ToInt32(txt_numero2.Text)).ToString();
+
+        }
+
+
+
+
+
+        private int multiplicar(int valor1, int valor2) //se puede colocar parametros em los parentesis
+        {
+            return valor1*valor2;
+        }
+
+        private void btn_multiplicar_Click(object sender, EventArgs e)
+        {
+
+            lbl_resultado.Text = multiplicar(Convert.ToInt32(txt_numero1.Text), Convert.ToInt32(txt_numero2.Text)).ToString();
+
+        }
+
+
+
+
+
+        private int dividir(int valor1, int valor2) //se puede colocar parametros em los parentesis
+        {
+            return valor1 / valor2;
+        }
+
+        private void btn_dividir_Click(object sender, EventArgs e)
+        {
+            lbl_resultado.Text = dividir(Convert.ToInt32(txt_numero1.Text), Convert.ToInt32(txt_numero2.Text)).ToString();
+
+        }
+    }
+}
